8786f1d3d67f96470773a69f082a912736cc2278
hubspotbot <oestrada@hubspot.com>
2025-09-25 22:39:12 -0400

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ToolEvaluationDashboard from './components/ToolEvaluationDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <ToolEvaluationDashboard />
+    </div>
+  );
+}
+
+export default App;
